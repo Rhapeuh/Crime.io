8f1bb1c2cda015ee320025955233339fe92b86cb
Adam Stievenard <adam.stievenard.etu@univ-lille.fr>
2026-03-13 12:19:07 +0100

    add function interfaces

--- server/types.ts
+++ server/types.ts
@@ -1,13 +1,34 @@
 export interface Joueur {
     pseudo?: string;
-    x: number;
-    y: number;
+    coJoueur: Coordonee
     vx: number;
     vy: number;
     speed: number;
+
+    getPseudo() : string;
+    setPseudo(nouveauPseudo: string) : void;
+
+    getX(): number;
+    getY(): number;
+    setX(posX : number): void;
+    setY(posY : number): void;
+
+    getVX(): number;
+    getVY(): number;
+    setVX(vecteurX : number): void;
+    setVY(vecteurY : number): void;
+
+    getSpeed() : number;
+    setSpeed(speed: number) : void;
 }
 
 export interface Coordonee {
     x: number;
     y: number;
+
+    getX(): number;
+    getY(): number;
+
+    setX(posX : number): void;
+    setY(posY : number): void;
 }
