e456a92f251479a1434bf7bf473b3d9d04f2e3ab
Adam Stievenard <adam.stievenard.etu>
2026-03-13 16:59:39 +0100

    render ennemy

--- client/src/JeuSoloView.ts
+++ client/src/JeuSoloView.ts
@@ -106,6 +106,6 @@ export default class JeuSoloView extends View {
 	private renderEnnemy(c: Coordonee){
 		const i = new Image();
 		i.src = '../public/img/ennemyTemp.jpg'
-		this.render
+		this.render(c, this.image)
 	}
 }
