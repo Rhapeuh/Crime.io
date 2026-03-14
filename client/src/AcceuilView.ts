947f67f1a296f9f8cdba448495b3d7ed5afd3620
rhapeuh <ylann.wattrelos.etu@univ-lille.fr>
2026-03-14 22:57:58 +0100

    ajout animation de tiroir

--- client/src/AcceuilView.ts
+++ client/src/AcceuilView.ts
@@ -1,9 +1,43 @@
-import View from './View';
 import Router from './Router';
+import View from './View';
 
 export default class AcceuilView extends View {
+	private menuGauche: HTMLElement;
+	private dernierElement: HTMLElement | null = null;
+
 	constructor(element: HTMLElement) {
 		super(element);
-		Router.setMenuElement(element);
+		this.menuGauche = this.element.querySelector('.colonne.gauche')!;
+		this.setMenuInterne();
+		Router.setMenuElement(this.element);
+	}
+
+	setMenuInterne() {
+		// On cible spécifiquement les boutons du menu
+		const menuLinks = this.element.querySelectorAll('.colonne.gauche > div');
+
+		menuLinks.forEach(element => {
+			element.addEventListener('click', event => {
+				event.preventDefault();
+
+				if (this.dernierElement === element) {
+					// Si on clique sur le même : on ferme
+					this.fermerTiroir();
+					this.dernierElement = null;
+				} else {
+					// Si on clique sur un autre : on ouvre (ou on reste ouvert)
+					this.ouvrirTiroir();
+					this.dernierElement = element as HTMLElement;
+				}
+			});
+		});
+	}
+
+	ouvrirTiroir() {
+		this.menuGauche.classList.add('ouvert');
+	}
+
+	fermerTiroir() {
+		this.menuGauche.classList.remove('ouvert');
 	}
 }
