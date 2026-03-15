7077fb78e749467af8bdf320ea019abe3c8ef7b5
rhapeuh <ylann.wattrelos.etu@univ-lille.fr>
2026-03-15 09:34:34 +0100

    ajout du fond + animation

--- client/src/AcceuilView.ts
+++ client/src/AcceuilView.ts
@@ -13,31 +13,43 @@ export default class AcceuilView extends View {
 	}
 
 	setMenuInterne() {
-		// On cible spécifiquement les boutons du menu
 		const menuLinks = this.element.querySelectorAll('.colonne.gauche > div');
 
 		menuLinks.forEach(element => {
 			element.addEventListener('click', event => {
 				event.preventDefault();
+				event.stopPropagation();
 
+				this.dernierElement?.classList.remove('btn-cliquer');
 				if (this.dernierElement === element) {
-					// Si on clique sur le même : on ferme
 					this.fermerTiroir();
-					this.dernierElement = null;
 				} else {
-					// Si on clique sur un autre : on ouvre (ou on reste ouvert)
-					this.ouvrirTiroir();
 					this.dernierElement = element as HTMLElement;
+					this.ouvrirTiroir();
 				}
 			});
+		});
+
+		document.addEventListener('click', event => {
+			const target = event.target as HTMLElement;
+
+			if (
+				this.menuGauche.classList.contains('ouvert') &&
+				!this.menuGauche.contains(target)
+			) {
+				this.fermerTiroir();
+			}
 		});
 	}
 
 	ouvrirTiroir() {
 		this.menuGauche.classList.add('ouvert');
+		this.dernierElement?.classList.add('btn-cliquer');
 	}
 
 	fermerTiroir() {
 		this.menuGauche.classList.remove('ouvert');
+		this.dernierElement?.classList.remove('btn-cliquer');
+		this.dernierElement = null;
 	}
 }
