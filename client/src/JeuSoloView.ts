87a20e0e9670ed4172dc4d343e915efbf86c6b1a
rhapeuh <ylann.wattrelos.etu@univ-lille.fr>
2026-03-21 11:41:05 +0100

    reglage du probleme de refresh in game

--- client/src/JeuSoloView.ts
+++ client/src/JeuSoloView.ts
@@ -1,9 +1,11 @@
 import { Socket } from 'socket.io-client';
 import JeuView from './JeuView';
+import Router from './Router';
 
 export default class JeuSoloView extends JeuView {
 	constructor(element: HTMLElement, socket: Socket, pseudo: string) {
 		super(element, socket);
+		if (pseudo === '') Router.navigate('/');
 		socket.emit('rejoindreSolo', pseudo);
 		this.socket.on('renderSolo', this.handleRender);
 	}
