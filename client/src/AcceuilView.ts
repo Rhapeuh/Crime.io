50d11dafe7c44620677031b8f5a7f6dac140d15b
Adam Stievenard <adam.stievenard.etu@univ-lille.fr>
2026-03-22 19:23:16 +0100

    combinaison des buttons et de la steam_pages

--- client/src/AcceuilView.ts
+++ client/src/AcceuilView.ts
@@ -3,7 +3,7 @@ import View from './View';
 import type { scores } from '../../common/types';
 import Score from './Score';
 import type { Socket } from 'socket.io-client';
-import Credit from './Credit';
+import { genererCredits, initEvents } from './Credits';
 
 export default class AcceuilView extends View {
 	private menuGauche: HTMLElement;
@@ -81,8 +81,10 @@ export default class AcceuilView extends View {
 		} else if (
 			this.dernierElement?.className.toLowerCase().includes('credit')
 		) {
-			this.fondElement.innerHTML = await Credit.genererCredit();
-
+			// credit button
+			this.fondElement.innerHTML = await genererCredits();
+			initEvents();
+			// credits steam
 		}
 	}
 
