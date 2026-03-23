58c1ea9e357492fa7d3fe6a0dcd095148e8a6c37
Adam Stievenard <adam.stievenard.etu>
2026-03-23 16:56:57 +0100

    suppression double credits

--- client/src/AcceuilView.ts
+++ client/src/AcceuilView.ts
@@ -3,7 +3,7 @@ import View from './View';
 import type { scores } from '../../common/types';
 import Score from './Score';
 import type { Socket } from 'socket.io-client';
-import { genererCredits, initEvents } from './Credits';
+import { genererCredits } from './Credits';
 
 export default class AcceuilView extends View {
 	private menuGauche: HTMLElement;
@@ -83,8 +83,6 @@ export default class AcceuilView extends View {
 		) {
 			// credit button
 			this.fondElement.innerHTML = await genererCredits();
-			initEvents();
-			// credits steam
 		}
 	}
 
