876c7360718246ee75a17edce611bbb30a852e1f
Adam Stievenard <adam.stievenard.etu>
2026-03-23 15:44:44 +0100

    ylann voulais ma version

--- client/src/Credits.ts
+++ client/src/Credits.ts
@@ -4,31 +4,32 @@ export async function genererCredits(): Promise<string> {
 }
 
 export function initEvents() {
-	const members = document.querySelectorAll('.team-member');
-	const detailsSection = document.querySelector(
-		'.member-details'
-	) as HTMLElement;
 
-	if (!detailsSection) return;
+	// const members = document.querySelectorAll('.team-member');
+	// const detailsSection = document.querySelector(
+	// 	'.member-details'
+	// ) as HTMLElement;
 
-	members.forEach(member => {
-		member.addEventListener('click', event => {
-			event.preventDefault();
-			members.forEach(m => m.classList.remove('active'));
-			member.classList.add('active');
+	// if (!detailsSection) return;
 
-			const iframes = document.querySelectorAll('.steamPage');
-			iframes.forEach((frame) => frame.setAttribute('class', 'steamPage'));
+	// members.forEach(member => {
+	// 	member.addEventListener('click', event => {
+	// 		event.preventDefault();
+	// 		members.forEach(m => m.classList.remove('active'));
+	// 		member.classList.add('active');
 
-			if (member == document.querySelector('[data-prenom="Ylann"]')) {
-				document.querySelector('.steamPage[src="/pageSteam/Rhapeuh.html"]')?.setAttribute('class', 'steamPage active');				
-			}
-			if (member == document.querySelector('[data-prenom="Adam"]')) {
-				document.querySelector('.steamPage[src="/pageSteam/YuNeria.html"]')?.setAttribute('class', 'steamPage active');			
-			}
-			if (member == document.querySelector('[data-prenom="Ethan"]')) {
-				document.querySelector('.steamPage[src="/pageSteam/Fleinz.html"]')?.setAttribute('class', 'steamPage active');		
-			}
-		});
-	});
+	// 		const iframes = document.querySelectorAll('.steamPage');
+	// 		iframes.forEach((frame) => frame.setAttribute('class', 'steamPage'));
+
+	// 		if (member == document.querySelector('[data-prenom="Ylann"]')) {
+	// 			document.querySelector('.steamPage[src="/pageSteam/Rhapeuh.html"]')?.setAttribute('class', 'steamPage active');				
+	// 		}
+	// 		if (member == document.querySelector('[data-prenom="Adam"]')) {
+	// 			document.querySelector('.steamPage[src="/pageSteam/YuNeria.html"]')?.setAttribute('class', 'steamPage active');			
+	// 		}
+	// 		if (member == document.querySelector('[data-prenom="Ethan"]')) {
+	// 			document.querySelector('.steamPage[src="/pageSteam/Fleinz.html"]')?.setAttribute('class', 'steamPage active');		
+	// 		}
+	// 	});
+	// });
 }
